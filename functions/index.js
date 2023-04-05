2c22d2bd4945d5cf4ca53cd7df66e9d830e2f171
Kim-kwan-woo <kw2577@naver.com>
2023-04-05 12:53:23 +0900

    전체 결과와 일정을 가져오는 함수 구현

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,10 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 const fetch = require("node-fetch");
 
+// 함수를 deploy 할 때는 "x-rapidapi-key"에 직접 값을 넣어야 함
 // 전체 팀 순위를 firebase에 저장
-exports.readStandings = onSchedule("every day 00:00", async (event) => {
+exports.readStandings = functions.pubsub.schedule("0 18 * * *")
+    .onRun(async (context) => {
       await fetch("https://v3.football.api-sports.io/standings?league=39&season=2022", {
         "method": "GET",
         "headers": {
@@ -23,11 +25,12 @@ exports.readStandings = onSchedule("every day 00:00", async (event) => {
             });
           })
           .catch((err) => console.log(err));
-});
+    });
 
 // 전체 팀 정보를 읽어 firestore에 저장
-exports.readTeams = onSchedule("every day 00:00", async (event) => {
-  await fetch("https://v3.football.api-sports.io/teams?league=39&season=2022", {
+exports.readTeams = functions.pubsub.schedule("0 18 * * *")
+    .onRun(async (context) => {
+      await fetch("https://v3.football.api-sports.io/teams?league=39&season=2022", {
         "method": "GET",
         "headers": {
           "x-rapidapi-host": "v3.football.api-sports.io",
@@ -45,10 +48,11 @@ exports.readTeams = onSchedule("every day 00:00", async (event) => {
             });
           })
           .catch((err) => console.log(err));
-});
+    });
 
 // 선수 득점 순위 가져와 파이어베이스에 저장
-exports.readPlayers = onSchedule("every day 00:00", async (event) => {
+exports.readPlayers = functions.pubsub.schedule("0 18 * * *")
+    .onRun(async (context) => {
       await fetch("https://v3.football.api-sports.io/players/topscorers?league=39&season=2022", {
         "method": "GET",
         "headers": {
@@ -65,25 +69,46 @@ exports.readPlayers = onSchedule("every day 00:00", async (event) => {
                 .set({topscorers: players});
           })
           .catch((err) => console.log(err));
-});
+    });
+
+// 전체 일정과 결과를 가져와 파이어베이스에 저장
+exports.readFixtures = functions.pubsub.schedule("0 18 * * *")
+    .onRun(async (context) => {
+      await fetch("https://v3.football.api-sports.io/fixtures?league=39&season=2022", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "v3.football.api-sports.io",
+          "x-rapidapi-key": process.env.API_KEY,
+        },
+      })
+          .then(async (response) => {
+            const data = await response.json();
+            const fixtures = data.response;
+            admin.firestore()
+                .collection("fixtures")
+                .doc("2022")
+                .set({fixtures: fixtures});
+          })
+          .catch((err) => console.log(err));
+    });
 
 // deploy 테스트 예시
-// exports.readPlayers = functions.pubsub.schedule("every 1 minutes")
+// exports.readFixtures = functions.pubsub.schedule("every 1 minutes")
 //     .onRun(async (context) => {
-//       await fetch("https://v3.football.api-sports.io/players/topscorers?league=39&season=2022", {
+//       await fetch("https://v3.football.api-sports.io/fixtures?league=39&season=2022", {
 //         "method": "GET",
 //         "headers": {
 //           "x-rapidapi-host": "v3.football.api-sports.io",
-//           "x-rapidapi-key": process.env.API_KEY,
+//           "x-rapidapi-key": "8df29bf352707d5a5ee8e084800d0e2a",
 //         },
 //       })
 //           .then(async (response) => {
 //             const data = await response.json();
-//             const players = data.response;
+//             const fixtures = data.response;
 //             admin.firestore()
-//                 .collection("players")
-//                 .doc("topscorers")
-//                 .set({topscorers: players});
+//                 .collection("fixtures")
+//                 .doc("2022")
+//                 .set({fixtures: fixtures});
 //           })
 //           .catch((err) => console.log(err));
 //     });
